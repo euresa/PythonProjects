a5e851c6b85f78f133c1c346d49bc2a420651a99
Sam Eure <eure.samuel@gmail.com>
2021-07-19 08:15:13 -0400

    Added Sandbox folder for playing around with stuff

---
+++ Sandbox/test/nearest.py
@@ -0,0 +1,7 @@
+def nearest_square(n):
+    """Returns the nearest perfect-square that is less than or equal to
+    a number 'n' given."""
+    if n < 1:
+        return 0
+    square_root = n ** 0.5
+    return int((square_root // 1) ** 2)
